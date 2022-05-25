298fa39b5835ed7b6f60d704265f6bcec5b4eea5
Edmund <edmund.adjei635@gmail.com>
2022-05-25 14:09:03 -0400

    commit tree bd20e0b4ee666aa555eff68c15ca13c6c18b6f77

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+let newHeader = document.createElement('h1')
+newHeader.id = "victory";
+newHeader.textContent = "Edmund Adjei is the champion";
+document.body.append(newHeader);
